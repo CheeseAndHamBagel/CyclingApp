dbb10b41134f7733619343355a52f52a1ce4f6db
Jude <92600970+CheeseAndHamBagel@users.noreply.github.com>
2022-03-01 17:10:02 +0000

    Add files via upload

---
+++ src/Rider.java
@@ -0,0 +1,60 @@
+public class Rider {
+    /**
+     * The rider class
+     * 
+     * 
+     */
+
+    private String name;
+    private int riderID;
+    private int yearOfBirth;
+    private int teamID;
+
+    /**
+     * Rider contructor
+     * 
+     * @param nameIN
+     * @param birthYr
+     * @param team
+     */
+    public Rider(String nameIN, int birthYr, int team){
+        name = nameIN;
+        yearOfBirth = birthYr;
+        teamID = team;
+    }
+    /**
+     * Returns the name of the rider
+     * 
+     * @return name
+     */
+    public String getName(){
+        return name;
+    }
+    
+    /**
+     * Sets the name of the rider to the string given
+     * 
+     * @param nameIN
+     */
+    public void setName(String nameIN){
+        name = nameIN;
+    }
+
+
+    
+    public int getYear(){
+        return yearOfBirth;
+    }
+
+    public void setYear(int year){
+        yearOfBirth = year;
+    }
+
+    public int getRiderID(){
+        return riderID;
+    }
+
+    public int getTeamID(){
+        return teamID;
+    }
+}
---
+++ src/Team.java
@@ -0,0 +1,12 @@
+public class Team {
+    /*
+    name
+    description
+    id
+    list of team members or member ids
+    */
+
+    private String name;
+    private String description;
+    private int teamID;
+}
