26fc042b7b0227a6a431b56be3adb4c30af11bf9
Jude <92600970+CheeseAndHamBagel@users.noreply.github.com>
2022-03-03 15:06:59 +0000

    Moved code into the cycling package

---
+++ src/cycling/Team.java
@@ -0,0 +1,29 @@
+public class Team {
+    /*
+    name
+    description
+    id
+    list of team members or member ids
+    */
+
+    private String name;
+    private String description;
+    private int teamID;
+    private int[] riders; //Official rules usually have 10-20 riders so will be instatiated to have 20 elements, even if most are empty
+
+    public String getName(){
+        return name;
+    }
+
+    public String getDesc(){
+        return description;
+    }
+
+    public int getTeamID(){
+        return teamID;
+    }
+
+    public int[] getRiders(){
+        return riders;
+    }
+}
