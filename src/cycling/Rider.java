304fc1796f39e2bf970680c98580ea4f7b224afb
Jude <92600970+CheeseAndHamBagel@users.noreply.github.com>
2022-03-24 16:24:59 +0000

    Added docstrings to Rider

--- src/cycling/Rider.java
+++ src/cycling/Rider.java
@@ -45,26 +45,50 @@ public class Rider implements Serializable{
         name = nameIN;
     }
 
+    /**
+     * Returns the year that rider was born in
+     * @return yearOfBirth
+     */
     public int getYear(){
         return yearOfBirth;
     }
 
+    /**
+     * Sets the riders year of birth to be the inputted integer
+     * @param year
+     */
     public void setYear(int year){
         yearOfBirth = year;
     }
 
+    /**
+     * returns thhe ID of the rider
+     * @return riderID
+     */
     public int getRiderID(){
         return riderID;
     }
 
+    /**
+     * returns the ID of the team that the rider is in
+     * @return teamID
+     */
     public int getTeamID(){
         return teamID;
     }
 
+    /**
+     * Sets the rider's team to the inputted team. To be used with addRider or removeRider to change the rider's team in the system
+     * @param newTeamID
+     */
     public void changeTeam(int newTeamID){
         teamID = newTeamID;
     }
 
+    /**
+     * returns a string of the details of the rider
+     * @return details
+     */
     public String toString(){
         return String.format("id=%d,name=%s,yob=%d,team=%d",riderID,name,yearOfBirth,teamID);
     }
